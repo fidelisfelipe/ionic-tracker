77b364d885c33fd6c2193d04956a1a3049b83822
Fidelis Guimaraes <fidelis.guimaraes@planejamento.gov.br>
2016-04-13 09:43:51 -0300

    add dependency's project blank

--- IonicAppTracker/www/js/app.js
+++ IonicAppTracker/www/js/app.js
@@ -3,22 +3,30 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic','ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaSms) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+	  
+//TODO: verify cordova.plugins	  undefined
+	  
+    if(window.cordova){//&& window.cordova.plugins.Keyboard) {//removed workawound
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	  //NOTE: This method started causing apps to be rejected from the App Store, so has been removed until a workaround is found.
+      //cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
       // Don't remove this line unless you know what you are doing. It stops the viewport
       // from snapping when text inputs are focused. Ionic handles this internally for
       // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+      //cordova.plugins.Keyboard.disableScroll(true);
+	  //$cordovaKeyboard.disableScroll(true);
+
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+	
   });
-})
+});
+
